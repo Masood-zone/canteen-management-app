4ec98e1c9e8cf4ddfb848866ef61ca989ae06d03
masood <masoodacheampong@gmail.com>
2024-11-23 09:46:52 +0000

    mock data for teachers, classes and students

---
+++ src/utils/mock/index.ts
@@ -0,0 +1,7 @@
+import TeachersData from "@/utils/mock/teachers-data.json";
+import ClassData from "@/utils/mock/classes-data.json";
+import StudentsData from "@/utils/mock/students-data.json";
+
+export const teachersList = TeachersData;
+export const classesList = ClassData;
+export const studentsList = StudentsData;
