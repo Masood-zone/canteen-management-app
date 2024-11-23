771c4d5b36788477addc69ce2d83e01d5833d634
masood <masoodacheampong@gmail.com>
2024-11-23 00:50:26 +0000

    done:completed working on the admin layout implementation with their various routes.

--- src/components/layout/admin-layout.tsx
+++ src/components/layout/admin-layout.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
+import { Toaster } from "../ui/sonner";
+import { SidebarInset, SidebarProvider } from "../ui/sidebar";
+import { AdminSidebar } from "../sidebar/admin-sidebar";
+import Navbar from "../navbar/admin-navbar";
 
 export default function AdminLayout() {
-  return <div>AdminLayout</div>;
+  return (
+    <SidebarProvider>
+      <AdminSidebar />
+      <SidebarInset>
+        <Navbar />
+        <main>
+          <Outlet />
+        </main>
+        <Toaster />
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
---
+++ src/components/sidebar/data.ts
@@ -0,0 +1,71 @@
+import { BookOpen, Bot, Settings2, SquareTerminal } from "lucide-react";
+
+export const super_nav = {
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/admin",
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: "Home",
+          url: "/admin",
+        },
+        {
+          title: "Teachers",
+          url: "/teachers",
+        },
+      ],
+    },
+    {
+      title: "Administartion",
+      url: "#",
+      icon: Bot,
+      items: [
+        {
+          title: "Students",
+          url: "/admin/students",
+        },
+        {
+          title: "Classes",
+          url: "/admin/classes",
+        },
+      ],
+    },
+    {
+      title: "Accounting",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Canteen Records",
+          url: "/admin/canteen-records",
+        },
+        {
+          title: "Payment History",
+          url: "/accounting/payment-history",
+        },
+      ],
+    },
+    {
+      title: "Settings",
+      url: "/settings",
+      icon: Settings2,
+      items: [
+        {
+          title: "General",
+          url: "/settings",
+        },
+        {
+          title: "Teachers",
+          url: "/settings/teachers",
+        },
+        {
+          title: "Notifications",
+          url: "#",
+        },
+      ],
+    },
+  ],
+};
