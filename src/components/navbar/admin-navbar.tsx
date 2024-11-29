49d50adc899642f1e7f81f828bc9c0c7638a6174
masood <masoodacheampong@gmail.com>
2024-11-29 23:31:22 +0000

    added time info to the top right of the app.

--- src/components/navbar/admin-navbar.tsx
+++ src/components/navbar/admin-navbar.tsx
@@ -1,8 +1,20 @@
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Breadcrumbs } from "./breadcrumb";
+import { formatDate, formatTime } from "@/utils/format-navbar-date";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  // Update time every second
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(timer); // Cleanup on unmount
+  }, []);
+
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       {/* Navbar */}
@@ -15,10 +27,12 @@ export default function Navbar() {
         </div>
         {/* Date, Time (24 hour countdown), day */}
         <div className="flex items-center gap-2">
-          <span className="text-sm text-gray-500">10:00 AM</span>
+          <span className="text-sm text-gray-500">
+            {formatTime(currentTime)}
+          </span>
           <Separator orientation="vertical" className="h-4" />
           <span className="text-sm text-gray-500">
-            Monday, 23rd November 2024
+            {formatDate(currentTime)}
           </span>
         </div>
       </div>
