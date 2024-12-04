5cba23d5929ebf262975f927c4190d9e894d9c31
masood <masoodacheampong@gmail.com>
2024-12-04 00:50:16 +0000

    setting up backend connection

--- src/services/root.ts
+++ src/services/root.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:3400",
+  baseURL: "https://canteen-management-backend.vercel.app",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
