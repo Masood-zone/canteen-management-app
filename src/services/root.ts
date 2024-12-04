0368fc9f3c638607f854a562689cba047fbaba23
masood <masoodacheampong@gmail.com>
2024-12-04 20:48:37 +0000

    worked on the frontend section of the app

--- src/services/root.ts
+++ src/services/root.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 export const apiClient = axios.create({
-  // baseURL: "https://canteen-management-backend.vercel.app",
-  baseURL: "http://localhost:3400",
+  baseURL: "https://canteen-management-backend.vercel.app",
+  // baseURL: "http://localhost:3400",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
